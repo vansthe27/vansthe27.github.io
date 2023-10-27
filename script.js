65b400300e259978d3cc0427bb3b2057427b54af
Camilo <100171469+vansthe27@users.noreply.github.com>
2023-10-27 10:23:23 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+     $('.scroll-up-btn').click(function(){
+         $('html').animate({scrollTop: 0});
+     });
+     
+    
+
+     var typed = new Typed(".typing", {
+         strings:["Desarrollador", "Diseñador","Ingeniero de Sistemas"],
+         typeSpeed:100,
+         backSpeed:60,
+         loop:true
+     });
+     var typed = new Typed(".typing-2", {
+        strings:["Desarrollador", "Diseñador","Ingeniero de Sistemas"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    });
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+     
+    let navLinks = document.querySelectorAll(".menu li a");
+    for(var i = 0; i < navLinks.length; i++){
+    navLinks[i].addEventListener("click" , ()=>{
+        $('.menu-btn').style.opacity = "1";
+        $('.menu-btn').style.pointerEventes = "auto";
+        $('.navbar').ClassList.remove("active");
+        body.style.overflow="auto";
+});
+}
+    $('.carousel').owlCarousel({
+        margin:20,
+        loop:true,
+        autoplayTimeOut:2000,
+        autoplayHoverPauser:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:false
+            }
+        }
+    });
+});
